09c8ae6e9595a8dfca3de55c36cbd07484e54988
Bruno R. Morillo <bruno@rmorillo.com>
2025-01-04 02:28:06 -0300

    fix: version

--- src/services/array.service.ts
+++ src/services/array.service.ts
@@ -1,6 +1,6 @@
-import { Normalize } from '../middleware/normalize.middleware';
+import { Injectable } from '@nestjs/common';
 
-@Normalize
+@Injectable()
 export class ArrayUtils {
   /**
    * Removes duplicate values from an array.
@@ -8,19 +8,20 @@ export class ArrayUtils {
    * @param keyFn Optional function to determine uniqueness based on a key.
    * @returns Array with unique values.
    * @example
-   * ArrayUtils.removeDuplicates({ array: [1, 2, 2, 3] }); // [1, 2, 3]
-   * ArrayUtils.removeDuplicates({
-   *   array: [{ id: 1 }, { id: 2 }, { id: 1 }],
-   *   keyFn: (item) => item.id,
-   * }); // [{ id: 1 }, { id: 2 }]
+   * ArrayUtils.removeDuplicates([1, 2, 2, 3]); // [1, 2, 3]
+   * ArrayUtils.removeDuplicates(
+   *   [{ id: 1 }, { id: 2 }, { id: 1 }],
+   *   (item) => item.id
+   * ); // [{ id: 1 }, { id: 2 }]
    */
-  public static removeDuplicates<T>({
-    array,
-    keyFn,
-  }: {
-    array: T[];
-    keyFn?: (item: T) => string | number;
-  }): T[] {
+  public static removeDuplicates<T>(
+    array: T[],
+    keyFn?: (item: T) => string | number,
+  ): T[] {
+    if (!Array.isArray(array)) {
+      throw new Error('Input must be an array');
+    }
+
     const seen = new Set();
     return keyFn
       ? array.filter((item) => {
@@ -38,15 +39,13 @@ export class ArrayUtils {
    * @param array2 Second array.
    * @returns Array containing values present in both arrays.
    * @example
-   * ArrayUtils.intersect({ array1: [1, 2, 3], array2: [2, 3, 4] }); // [2, 3]
+   * ArrayUtils.intersect([1, 2, 3], [2, 3, 4]); // [2, 3]
    */
-  public static intersect<T>({
-    array1,
-    array2,
-  }: {
-    array1: T[];
-    array2: T[];
-  }): T[] {
+  public static intersect<T>(array1: T[], array2: T[]): T[] {
+    if (!Array.isArray(array1) || !Array.isArray(array2)) {
+      throw new Error('Both inputs must be arrays');
+    }
+
     const set2 = new Set(array2);
     return array1.filter((value) => set2.delete(value));
   }
@@ -56,9 +55,13 @@ export class ArrayUtils {
    * @param array Multi-dimensional array.
    * @returns Flattened array.
    * @example
-   * ArrayUtils.flatten({ array: [1, [2, [3, 4]], 5] }); // [1, 2, 3, 4, 5]
+   * ArrayUtils.flatten([1, [2, [3, 4]], 5]); // [1, 2, 3, 4, 5]
    */
-  public static flatten<T>({ array }: { array: (T | T[])[] }): T[] {
+  public static flatten<T>(array: (T | T[])[]): T[] {
+    if (!Array.isArray(array)) {
+      throw new Error('Input must be an array');
+    }
+
     const result: T[] = [];
     const stack = [...array];
 
@@ -80,22 +83,19 @@ export class ArrayUtils {
    * @param keyFn Function to determine the group key.
    * @returns An object with keys as group identifiers and values as arrays of grouped elements.
    * @example
-   * ArrayUtils.groupBy({
-   *   array: [
-   *     { type: 'fruit', name: 'apple' },
-   *     { type: 'fruit', name: 'banana' },
-   *     { type: 'vegetable', name: 'carrot' },
-   *   ],
-   *   keyFn: (item) => item.type,
-   * }); // { fruit: [...], vegetable: [...] }
+   * ArrayUtils.groupBy(
+   *   [{ type: 'fruit', name: 'apple' }, { type: 'vegetable', name: 'carrot' }],
+   *   (item) => item.type
+   * ); // { fruit: [...], vegetable: [...] }
    */
-  public static groupBy<T>({
-    array,
-    keyFn,
-  }: {
-    array: T[];
-    keyFn: (item: T) => string | number;
-  }): Record<string | number, T[]> {
+  public static groupBy<T>(
+    array: T[],
+    keyFn: (item: T) => string | number,
+  ): Record<string | number, T[]> {
+    if (!Array.isArray(array)) {
+      throw new Error('Input must be an array');
+    }
+
     return array.reduce(
       (acc, item) => {
         const key = keyFn(item);
@@ -112,10 +112,9 @@ export class ArrayUtils {
    * @param array Array to be shuffled.
    * @returns New array with shuffled elements.
    * @example
-   * ArrayUtils.shuffle({ array: [1, 2, 3, 4] }); // [3, 1, 4, 2]
-   * ArrayUtils.shuffle({ array: ['a', 'b', 'c'] }); // ['b', 'c', 'a']
+   * ArrayUtils.shuffle([1, 2, 3, 4]); // [3, 1, 4, 2]
    */
-  public static shuffle<T>({ array }: { array: T[] }): T[] {
+  public static shuffle<T>(array: T[]): T[] {
     if (!Array.isArray(array)) {
       throw new Error('Input must be an array');
     }
@@ -131,30 +130,17 @@ export class ArrayUtils {
   /**
    * Sorts an array based on the specified order and key(s).
    * @param array The array to be sorted.
-   * @param orderBy Sorting criteria. Can be:
-   *   - A string: `'asc'` or `'desc'` for arrays of primitive types.
-   *   - An object: `{ key: 'asc' | 'desc' }` or `{ key1: 'asc', key2: 'desc' }` for arrays of objects.
+   * @param orderBy Sorting criteria.
    * @returns A new array sorted based on the specified criteria.
    * @example
-   * ArrayUtils.sort({ array: ['c', 'z', 'a'], orderBy: 'asc' }); // ['a', 'c', 'z']
-   * ArrayUtils.sort({
-   *   array: [
-   *     { id: 1, name: 'bruno' },
-   *     { id: 2, name: 'andrea' },
-   *     { id: 3, name: 'Débora' },
-   *   ],
-   *   orderBy: { name: 'asc' }
-   * }); // Sorted by name in ascending order
+   * ArrayUtils.sort(['c', 'a', 'b'], 'asc'); // ['a', 'b', 'c']
    */
-  public static sort<T>({
-    array,
-    orderBy,
-  }: {
-    array: T[];
-    orderBy: 'asc' | 'desc' | Record<string, 'asc' | 'desc'>;
-  }): T[] {
+  public static sort<T>(
+    array: T[],
+    orderBy: 'asc' | 'desc' | Record<string, 'asc' | 'desc'>,
+  ): T[] {
     if (!Array.isArray(array) || array.length === 0) {
-      return array;
+      throw new Error('Input must be a non-empty array');
     }
 
     const isPrimitive = typeof array[0] !== 'object';
@@ -165,7 +151,7 @@ export class ArrayUtils {
       );
     }
 
-    if (typeof orderBy === 'object' && !Array.isArray(orderBy)) {
+    if (typeof orderBy === 'object') {
       const keys = Object.keys(orderBy);
 
       return [...array].sort((a, b) => {
@@ -184,74 +170,5 @@ export class ArrayUtils {
     throw new Error(
       "Invalid 'orderBy' format. Use 'asc', 'desc', or an object specifying keys and orders.",
     );
-  }
-
-  /**
-   * Finds the first object in an array where the subset matches the superset.
-   * @template T Type of the objects in the array.
-   * @param array Array of objects to search.
-   * @param subset Object containing key-value pairs to match.
-   * @returns The first object matching the subset, or `null` if not found.
-   * @example
-   * ArrayUtils.findSubset({
-   *   array: [
-   *     { id: 1, name: 'John' },
-   *     { id: 2, name: 'Jane' },
-   *   ],
-   *   subset: { name: 'John' },
-   * }); // { id: 1, name: 'John' }
-   */
-  public static findSubset<T extends Record<string, any>>({
-    array,
-    subset,
-  }: {
-    array: T[];
-    subset: Partial<T>;
-  }): T | null {
-    return (
-      array.find((item) =>
-        Object.entries(subset).every(([key, value]) => {
-          if (value === undefined) return true;
-          if (Array.isArray(value)) {
-            return (
-              Array.isArray(item[key]) &&
-              value.every((val) => (item[key] as any[]).includes(val))
-            );
-          }
-          return item[key] === value;
-        }),
-      ) || null
-    );
-  }
-
-  /**
-   * Checks if a subset is fully contained within a superset.
-   * @template T Type of the objects being compared.
-   * @param superset The object to check against.
-   * @param subset The subset to validate.
-   * @returns `true` if the superset contains the subset, otherwise `false`.
-   * @example
-   * ArrayUtils.isSubset({
-   *   superset: { id: 1, name: 'John' },
-   *   subset: { name: 'John' },
-   * }); // true
-   */
-  public static isSubset<T extends Record<string, any>>({
-    superset,
-    subset,
-  }: {
-    superset: T;
-    subset: Partial<T>;
-  }): boolean {
-    return Object.entries(subset).every(([key, value]) => {
-      if (value === undefined) return true;
-      if (Array.isArray(value)) {
-        return (
-          Array.isArray(superset[key]) &&
-          value.every((val) => (superset[key] as any[]).includes(val))
-        );
-      }
-      return superset[key] === value;
-    });
   }
 }
