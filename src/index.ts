998ae0b2062d8f786693342319347c94f056dc78
Bruno R. Morillo <bruno@rmorillo.com>
2025-01-07 15:43:53 -0300

    fix structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export const helloWorld = (): string => 'Hello, World!';
+
+export function sum(a: number, b: number): void {
+  console.log(`The sum is: ${a + b}`);
+}
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: [
-    'src/index.ts', // Arquivo principal da sua biblioteca
-    // Adicione outras entradas, como módulos ou localizações
-  ],
-  outDir: 'dist', // Diretório de saída
-  clean: true, // Limpa o diretório de saída antes de cada build
-  format: ['esm', 'cjs'], // Gera formatos ESM e CommonJS
-  target: ['es2022', 'node18'], // Define a versão alvo
-  dts: true, // Gera declarações de tipos (.d.ts)
-  minify: true, // Minifica o código
-  sourcemap: true, // Gera sourcemaps
-  splitting: true, // Habilita code splitting
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'], // Gera CommonJS (.cjs) e ESM (.mjs)
+  outExtension: ({ format }) =>
+    format === 'cjs' ? { js: '.cjs' } : { js: '.mjs' }, // Extensões personalizadas
+  dts: true, // Gera tipos TypeScript
+  clean: true, // Limpa a pasta dist antes de compilar
 });
